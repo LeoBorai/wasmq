3710f217f2dd083ccbde11f702a18eeab42eaea1
Leo Borai <estebanborai@gmail.com>
2026-01-18 15:33:46 +0100

    feat: use `tracing` for internal logging

--- src/config/src/hub.rs
+++ src/config/src/hub.rs
@@ -1,6 +1,8 @@
+use std::net::SocketAddr;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct HubConfig {
-    pub api_addr: String,
+    pub api_addr: SocketAddr,
 }
