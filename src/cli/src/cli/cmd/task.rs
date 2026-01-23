c8fe8f1cae64e17f78146f8b07111bd8f591f5ea
Leo Borai <estebanborai@gmail.com>
2026-01-23 22:24:52 +0100

    feat: setup mate task

--- src/cli/src/cli/cmd/task.rs
+++ src/cli/src/cli/cmd/task.rs
@@ -1,12 +1,16 @@
+mod new;
 mod run;
 
 use anyhow::Result;
 use clap::Parser;
 
+use self::new::TaskNewOpt;
 use self::run::TaskRunOpt;
 
 #[derive(Debug, Parser)]
 pub enum TaskCmd {
+    /// Create a new Task project
+    New(TaskNewOpt),
     /// Run a task passing arguments and retrieves results
     Run(TaskRunOpt),
 }
@@ -14,6 +18,7 @@ pub enum TaskCmd {
 impl TaskCmd {
     pub async fn exec(&self) -> Result<()> {
         match &self {
+            TaskCmd::New(cmd) => cmd.exec().await,
             TaskCmd::Run(cmd) => cmd.exec().await,
         }
     }
