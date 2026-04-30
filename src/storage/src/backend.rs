e655fb742bd56946cab4230d52692f7cb2798df0
Leo Borai <estebanborai@gmail.com>
2026-04-30 18:53:49 +0200

    feat: use `Ulid` over `Uuid`

--- src/storage/src/backend.rs
+++ src/storage/src/backend.rs
@@ -2,7 +2,7 @@ pub mod sqlite;
 
 use anyhow::Result;
 use async_trait::async_trait;
-use uuid::Uuid;
+use ulid::Ulid;
 
 use mate::proto::job::{Job, JobQuery, JobResult};
 
@@ -10,6 +10,6 @@ use mate::proto::job::{Job, JobQuery, JobResult};
 pub trait Backend: Send + Sync {
     async fn create_job(&self, job: Job) -> Result<Job>;
     async fn retrieve_jobs(&self, query: JobQuery) -> Result<Vec<Job>>;
-    async fn update_job_completed(&self, id: Uuid, result: JobResult) -> Result<()>;
-    async fn claim_job(&self, job_id: Uuid, claimed_by: String) -> Result<Job>;
+    async fn update_job_completed(&self, id: Ulid, result: JobResult) -> Result<()>;
+    async fn claim_job(&self, job_id: Ulid, claimed_by: String) -> Result<Job>;
 }
